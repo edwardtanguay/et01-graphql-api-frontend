6d780f328c3a72b772fa69ea67b988d6f4f50dd8
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-30 17:53:58 +0200

    apollo in

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import './App.scss'
+import './App.scss';
+import { useQuery, gql } from '@apollo/client';
+
+const QUERY = gql`
+	{
+		launchesPast(limit: 10) {
+			id
+			mission_name
+		}
+	}
+`;
 
 function App() {
+	const { data, loading, error } = useQuery(QUERY);
 
-  return (
-    <div className="App">
-    <h1>GraphQL Test - Frontend</h1>
-    <p>nnn</p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>GraphQL Test - Frontend</h1>
+			<ul>
+				{data.launchesPast.map((launch:any, i: number) => (
+					<li key={i}>{launch.mission_name}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql',
+    cache: new InMemoryCache()
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
